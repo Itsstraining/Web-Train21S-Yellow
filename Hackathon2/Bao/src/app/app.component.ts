033f98f8774bf955b1f9efd1e86f5cebfc57cea3
LeeThanhBao <bao.lt5030@sinhvien.hoasen.edu.vn>
2021-01-22 20:47:33 +0700

    Excel Hackathon cua Bao

---
+++ Hackathon2/Bao/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'excel';
+  style="width:1000px;  overflow:hidden;  box-shadow:0 0 20px rgba(0,0,0);"
+}

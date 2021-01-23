eef36cd7544c6bcf0f457f08c73cbc7eba8a2d7f
Huynh Duc <hsu.tranhuynhduc@gmail.com>
2021-01-23 17:49:22 +0700

    update

--- Hackathon2/Phu/Hackathon 2 - 1/src/app/app-routing.module.ts
+++ Hackathon2/Phu/Hackathon 2 - 1/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: 'Home', loadChildren: () => import('./Pages/home/home.module').then(m => m.HomeModule) }, { path: 'Intro', loadChildren: () => import('./Pages/intro/intro.module').then(m => m.IntroModule) }];
+const routes: Routes = [
+  { path: 'Home', loadChildren: () => import('./Pages/home/home.module').then(m => m.HomeModule) },
+  { path: '', loadChildren: () => import('./Pages/intro/intro.module').then(m => m.IntroModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
